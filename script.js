00d5613dbb29889aa5aa9a66e8d16242c53043e6
ryanschuttig <ryanschuttig@gmail.com>
2020-06-10 20:17:32 -0400

    Added variables

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  var numOfChar = prompt("How many Characters")
+  var numOfChar = prompt("How many characters do you want?");
+  var confirmUpper = confirm("Do you want uppercase letters?");
+  var confirmLower = confirm("Do you want lowercase letters?");
+  var confirmNumbers = confirm("Do you want numbers?");
+  var confirmSpecial = confirm("Do you want special characters?");
+  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var symbols = "!@#$%^&*=-_"
+  var arrayUpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var arrayLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var arrayNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var arraySymbols = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_"]
 
 
 
 
-  return "numOfChar"
+  return "randomPassword"
 }
 
 

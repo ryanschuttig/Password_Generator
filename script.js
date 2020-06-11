556a8107eb9a7769049b7c35cbc06ad08e73ded9
ryanschuttig <ryanschuttig@gmail.com>
2020-06-11 19:54:07 -0400

    Randomized password

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Assignment Code
-// Hooks into button
+// Hooks to Button
 var generateBtn = document.querySelector("#generate");
 // GLOBALS
 var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -13,61 +12,49 @@ var passCriteria = [];
 var password = "";
 var finalPassword = "";
 
-// Build Password
+// Builds Password
 function generatePassword() {
 
-  // Ask user how many characters
+  // Asks user how many characters in password
   var numOfChar = prompt("How many characters do you want?");
-  // Checking if number of characters in range
-  if (numOfChar > 7 && numOfChar < 27) {
-    // Ask for everything they want
+  // If character input less than 8 or more than 128, ask again
+  if (numOfChar > 7 && numOfChar < 127) {
+    // Asks user for each type of character they want
     var confirmUpper = confirm("Do you want uppercase letters?");
     var confirmLower = confirm("Do you want lowercase letters?");
     var confirmNumbers = confirm("Do you want numbers?");
     var confirmSpecial = confirm("Do you want special characters?");
 
-    // Include everything asked for
+    // Includes everything asked for in password
     if (confirmUpper) {
       password += passCriteria.concat(arrayUpperChar);
       console.log(password)
-    }
-    else {
-      return false
     }
 
     if (confirmLower) {
       password += passCriteria.concat(arrayLowerChar);
       console.log(password)
     }
-    else {
-      return false
-    }
 
     if (confirmNumbers) {
       password += passCriteria.concat(arrayNumbers);
       console.log(password)
-    }
-    else {
-      return false
     }
 
     if (confirmSpecial) {
       password += passCriteria.concat(arraySpecial);
       console.log(password)
     }
-    else {
-      return false
-    }
-
-    password = password.replace(/,/g , "")
-    console.log("stripped password" , password)
+    // Replaces all array commas
+    password = password.replace(/,/g, "")
+    console.log("stripped password", password)
+    // Loops random generated password between input choices
     for (let index = 0; index < numOfChar; index++) {
       finalPassword += password.charAt(Math.floor(Math.random() * password.length));
-      
     }
+    // Returns final randomized password
     console.log(password)
     return finalPassword
-    // [Math.floor(Math.random * numOfChar)])
 
   } else
     alert("Must be between 8 and 128 characters")
@@ -82,8 +69,3 @@ function writePassword() {
 }
 // Add event listener to generate button - fires function writePassword
 generateBtn.addEventListener("click", writePassword);
-
-
-//Research concatenate method
-//Array of upper or lower case letters
-//Containers of everything -> send content into 1 big container to build password
